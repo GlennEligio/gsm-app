8760a7b7d04e1a735dea3b5aac2a2ccfff856ba3
John Glenn Eligio <glenneligio@gmail.com>
2022-08-12 22:14:24 +0800

    Initial commit

---
+++ GSM/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace GSM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtSendMessage = new System.Windows.Forms.TextBox();
+            this.btnSendMessage = new System.Windows.Forms.Button();
+            this.cboxPorts = new System.Windows.Forms.ComboBox();
+            this.txtPhoneNum = new System.Windows.Forms.TextBox();
+            this.timerGsmMessagePoll = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtReceivedMessages = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnReadMessage = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Phone number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 104);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(61, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Message";
+            // 
+            // txtSendMessage
+            // 
+            this.txtSendMessage.Location = new System.Drawing.Point(162, 153);
+            this.txtSendMessage.Multiline = true;
+            this.txtSendMessage.Name = "txtSendMessage";
+            this.txtSendMessage.Size = new System.Drawing.Size(201, 112);
+            this.txtSendMessage.TabIndex = 5;
+            // 
+            // btnSendMessage
+            // 
+            this.btnSendMessage.Location = new System.Drawing.Point(162, 282);
+            this.btnSendMessage.Name = "btnSendMessage";
+            this.btnSendMessage.Size = new System.Drawing.Size(114, 23);
+            this.btnSendMessage.TabIndex = 6;
+            this.btnSendMessage.Text = "Send Message";
+            this.btnSendMessage.UseVisualStyleBackColor = true;
+            this.btnSendMessage.Click += new System.EventHandler(this.btnSendMessage_Click);
+            // 
+            // cboxPorts
+            // 
+            this.cboxPorts.FormattingEnabled = true;
+            this.cboxPorts.Location = new System.Drawing.Point(162, 101);
+            this.cboxPorts.Name = "cboxPorts";
+            this.cboxPorts.Size = new System.Drawing.Size(201, 21);
+            this.cboxPorts.TabIndex = 7;
+            this.cboxPorts.SelectedValueChanged += new System.EventHandler(this.cboxPorts_SelectedValueChanged);
+            // 
+            // txtPhoneNum
+            // 
+            this.txtPhoneNum.Location = new System.Drawing.Point(162, 50);
+            this.txtPhoneNum.Name = "txtPhoneNum";
+            this.txtPhoneNum.Size = new System.Drawing.Size(201, 20);
+            this.txtPhoneNum.TabIndex = 3;
+            // 
+            // timerGsmMessagePoll
+            // 
+            this.timerGsmMessagePoll.Interval = 1000;
+            this.timerGsmMessagePoll.Tick += new System.EventHandler(this.timerGsmMessagePoll_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(460, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Messages";
+            // 
+            // txtReceivedMessages
+            // 
+            this.txtReceivedMessages.Location = new System.Drawing.Point(463, 78);
+            this.txtReceivedMessages.Multiline = true;
+            this.txtReceivedMessages.Name = "txtReceivedMessages";
+            this.txtReceivedMessages.ReadOnly = true;
+            this.txtReceivedMessages.Size = new System.Drawing.Size(304, 187);
+            this.txtReceivedMessages.TabIndex = 9;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(162, 325);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(114, 23);
+            this.btnConnect.TabIndex = 10;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(296, 325);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(114, 23);
+            this.btnDisconnect.TabIndex = 11;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnReadMessage
+            // 
+            this.btnReadMessage.Location = new System.Drawing.Point(463, 282);
+            this.btnReadMessage.Name = "btnReadMessage";
+            this.btnReadMessage.Size = new System.Drawing.Size(140, 23);
+            this.btnReadMessage.TabIndex = 12;
+            this.btnReadMessage.Text = "Read messages";
+            this.btnReadMessage.UseVisualStyleBackColor = true;
+            this.btnReadMessage.Click += new System.EventHandler(this.btnReadMessage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 360);
+            this.Controls.Add(this.btnReadMessage);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtReceivedMessages);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cboxPorts);
+            this.Controls.Add(this.btnSendMessage);
+            this.Controls.Add(this.txtSendMessage);
+            this.Controls.Add(this.txtPhoneNum);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.btnConnect_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtSendMessage;
+        private System.Windows.Forms.Button btnSendMessage;
+        private System.Windows.Forms.ComboBox cboxPorts;
+        private System.Windows.Forms.TextBox txtPhoneNum;
+        private System.Windows.Forms.Timer timerGsmMessagePoll;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtReceivedMessages;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnReadMessage;
+    }
+}
+
---
+++ GSM/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GSM
+{
+    public partial class Form1 : Form
+    {
+        private readonly string AT = "AT" + Environment.NewLine;
+        private readonly string SMS_MODE = "AT+CMGF=1" + Environment.NewLine;
+        private readonly string GSM_TE_SET = "AT+CSCS=\"GSM\"" + Environment.NewLine;
+        private readonly string READ_MESSAGE = "AT+CMGL" + Environment.NewLine;
+        private string gsmPortNumber = "";
+        private SerialPort gsmPort;
+        private bool isConnected = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            PopulateComboBoxWithPorts();
+        }
+
+        private string setPhoneNumberString(string message)
+        {
+            return "AT+CMGS=\"" + message + "\"" + Environment.NewLine;
+        }
+
+        private void PopulateComboBoxWithPorts()
+        {
+            foreach (string portname in SerialPort.GetPortNames())
+            {
+                cboxPorts.Items.Add(portname);
+            }
+        }
+
+        private bool Connect()
+        {
+            Console.WriteLine("Connecting");
+            if (gsmPort == null || !isConnected || gsmPort.IsOpen)
+            {
+                gsmPort = new SerialPort();
+                if (gsmPortNumber == null || gsmPortNumber == "")
+                {
+                    MessageBox.Show("No port specified", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+
+                try
+                {
+                    gsmPort.PortName = gsmPortNumber;
+                    gsmPort.Open();
+                    isConnected = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    isConnected = false;
+                }
+
+            }
+
+            return isConnected;
+        }
+
+        public void Disconnect()
+        {
+            if (gsmPort != null || isConnected || gsmPort.IsOpen)
+            {
+                gsmPort.Close();
+                gsmPort.Dispose();
+                isConnected = false;
+            }
+        }
+
+        private void Send(string toAddress, string message)
+        {
+            Console.WriteLine("Sending..");
+
+            gsmPort.WriteLine(AT);
+            Thread.Sleep(100);
+            gsmPort.WriteLine("AT+CMGF=1" + Environment.NewLine); // Set mode to Text(1) or PDU(0)
+            Thread.Sleep(100);
+            gsmPort.WriteLine(GSM_TE_SET);
+            Thread.Sleep(100);
+            gsmPort.WriteLine("AT+CMGS=\"" + toAddress + "\"" + Environment.NewLine);
+            Thread.Sleep(100);
+            gsmPort.Write(message);
+            gsmPort.Write(new byte[] { 26 }, 0, 1);
+            Thread.Sleep(100);
+
+            string response = gsmPort.ReadExisting();
+
+            if (response.EndsWith("\r\nOK\r\n") && response.Contains("+CMGS:")) // IF CMGS IS MISSING IT MEANS THE MESSAGE WAS NOT SENT!
+            {
+                Console.WriteLine(response);
+                // add more code here to manipulate reponse string.
+            }
+            else
+            {
+                // add more code here to handle error.
+                Console.WriteLine(response);
+            }
+        }
+
+
+        private string Read()
+        {
+            Console.WriteLine("Reading..");
+
+            gsmPort.WriteLine("AT+CMGF=1"); // Set mode to Text(1) or PDU(0)
+            Thread.Sleep(1000); // Give a second to write
+            gsmPort.WriteLine("AT+CPMS=\"SM\""); // Set storage to SIM(SM)
+            Thread.Sleep(1000);
+            gsmPort.WriteLine("AT+CMGL=\"ALL\""); // What category to read ALL, REC READ, or REC UNREAD
+            Thread.Sleep(1000);
+            gsmPort.Write("\r");
+            Thread.Sleep(1000);
+
+            string response = gsmPort.ReadExisting();
+
+            if (response.EndsWith("\r\nOK\r\n"))
+            {
+                Console.WriteLine(response);
+                return response;
+            }
+            else
+            {
+                // add more code here to handle error.
+                Console.WriteLine(response);
+                return null;
+            }
+        }
+
+        private void btnSendMessage_Click(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                string phoneAddress = txtPhoneNum.Text;
+                string message = txtSendMessage.Text;
+                Send(phoneAddress, message);
+
+            }
+        }
+
+        private void timerGsmMessagePoll_Tick(object sender, EventArgs e)
+        {
+            if (isConnected)
+            {
+                string receivedMessages = Read();
+                if (receivedMessages != null)
+                {
+                    txtReceivedMessages.AppendText(receivedMessages);
+                }
+            }
+        }
+
+        private void cboxPorts_SelectedValueChanged(object sender, EventArgs e)
+        {
+            ComboBox cbox = sender as ComboBox;
+            if (cbox != null)
+            {
+                gsmPortNumber = cbox.Text;
+            }
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            Connect();
+            if (isConnected)
+            {
+                MessageBox.Show("Connected", "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Connection failed", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            Disconnect();
+        }
+
+        private void btnReadMessage_Click(object sender, EventArgs e)
+        {
+            if(isConnected)
+            {
+                string messages = Read();
+                if(messages != null)
+                {
+                    txtReceivedMessages.AppendText(messages);
+                }
+            }
+        }
+    }
+}
